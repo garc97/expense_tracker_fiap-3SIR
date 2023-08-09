c7c957e46b638c11e86575b99f4190820d3d4464
garc97 <68360711+garc97@users.noreply.github.com>
2023-08-08 22:27:56 -0300

    contas ok

--- lib/pages/contas_page.dart
+++ lib/pages/contas_page.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/models/banco.dart';
+import 'package:expense_tracker/repository/contas_repository.dart';
 import 'package:flutter/material.dart';
 
 class ContasPage extends StatefulWidget {
@@ -8,10 +10,29 @@ class ContasPage extends StatefulWidget {
 }
 
 class _ContasPageState extends State<ContasPage> {
+  final contas = ContasRepository().listarConta();
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Contas Page'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Contas'),
+        ),
+        body: ListView.separated(
+          itemCount: contas.length,
+          itemBuilder: (context, index) {
+            final conta = contas[index];
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage:
+                    AssetImage("images/" + bancosMap[conta.bancoId]!.logo),
+              ),
+              title: Text(conta.descricao),
+              subtitle: Text(conta.tipoConta.name),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },
+        ));
   }
 }
